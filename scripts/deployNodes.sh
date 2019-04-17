5ffb6c79040a14253296d9b83d897b4cbb076e6a
Eric McEvoy <ericmcevoy@19LDN-MAC109.local>
2019-04-17 13:58:21 +0100

    added membership conf and cordapps to relevant node folders

--- scripts/deployNodes.sh
+++ scripts/deployNodes.sh
@@ -4,9 +4,20 @@ cd ../
 
 killall java -9
 
-rm -rf kotlin-source/build/nodes
+rm -rf build/nodes
 
 ./gradlew clean
 ./gradlew deployNodes
 ./gradlew --stop
+
+cp membership-service.conf build/nodes/Notary/cordapps/config/.
+
+cp membership-service.conf build/nodes/Worldpay_BNO/cordapps/config/.
+
+cp membership-service.conf build/nodes/PartyA/cordapps/config/.
+
+cp membership-service.conf build/nodes/PartyB/cordapps/config/.
+
+cp membership-service.conf build/nodes/PartyC/cordapps/config/.
+
 
