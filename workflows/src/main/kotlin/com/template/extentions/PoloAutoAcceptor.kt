e01a49df0462189e1129f6597ba55aff8f596316
Eric McEvoy <ericmcevoy@19LDN-MAC109.local>
2019-04-17 13:59:22 +0100

    configured auto acceptor by overriding requestmembershipresponder flow

--- workflows/src/main/kotlin/com/template/extentions/PoloAutoAcceptor.kt
+++ workflows/src/main/kotlin/com/template/extentions/PoloAutoAcceptor.kt
@@ -1,2 +1,24 @@
 package com.template.extentions
 
+import co.paralleluniverse.fibers.Suspendable
+import com.r3.businessnetworks.membership.flows.bno.RequestMembershipFlowResponder
+import com.r3.businessnetworks.membership.flows.bno.service.BNOConfigurationService
+import com.r3.businessnetworks.membership.flows.member.RequestMembershipFlow
+import com.r3.businessnetworks.membership.states.MembershipState
+import net.corda.core.flows.FlowSession
+import net.corda.core.flows.InitiatedBy
+
+
+ /*
+  * This class uses flow overrides to automatically accept memberships
+  * This can be deployed as a separate jar to change whether the BNO auto accepts or not
+  * See: https://docs.corda.net/head/flow-overriding.html
+  */
+
+@InitiatedBy(RequestMembershipFlow::class)
+class PoloAutoAcceptor(otherSideSession: FlowSession) : RequestMembershipFlowResponder(otherSideSession){
+    @Suspendable
+    override fun activateRightAway(membershipState : MembershipState<Any>, configuration : BNOConfigurationService) : Boolean {
+        return true
+    }
+}
