8944ceb3b6d92d712e8aa1b603a10c018d8478f5
Eric McEvoy <ericmcevoy@19ldn-mac109.home>
2019-04-17 23:50:09 +0100

    updated scripts

--- scripts/deployNodes.sh
+++ scripts/deployNodes.sh
@@ -11,13 +11,18 @@ rm -rf build/nodes
 ./gradlew --stop
 
 cp membership-service.conf build/nodes/Notary/cordapps/config/.
+cp billing-app.conf build/nodes/Notary/cordapps/config/.
 
 cp membership-service.conf build/nodes/Worldpay_BNO/cordapps/config/.
+cp billing-app.conf build/nodes/Worldpay_BNO/cordapps/config/.
 
 cp membership-service.conf build/nodes/PartyA/cordapps/config/.
+cp billing-app.conf build/nodes/PartyA/cordapps/config/.
 
 cp membership-service.conf build/nodes/PartyB/cordapps/config/.
+cp billing-app.conf build/nodes/PartyB/cordapps/config/.
 
 cp membership-service.conf build/nodes/PartyC/cordapps/config/.
+cp billing-app.conf build/nodes/PartyC/cordapps/config/.
 
 
--- scripts/requestBusinessNetworkMembership.sh
+++ scripts/requestBusinessNetworkMembership.sh
@@ -1,1 +1,28 @@
 #!/usr/bin/env bash
+
+echo "Request membership for all nodes in Business Network"
+
+curl -X POST \
+  http://localhost:50005/api/bnm/requestMembership \
+  -H 'Content-Type: application/json' \
+  -H 'alternativeName: PartyA' \
+  -H 'cache-control: no-cache' \
+  -H 'role: Party Alpha' \
+
+curl -X POST \
+  http://localhost:50006/api/bnm/requestMembership \
+  -H 'alternativeName: PartyB' \
+  -H 'cache-control: no-cache' \
+  -H 'role: Party Bravo'
+
+curl -X POST \
+  http://localhost:50007/api/bnm/requestMembership \
+  -H 'alternativeName: PartyC' \
+  -H 'cache-control: no-cache' \
+  -H 'role: Party Charlie'
+
+
+
+
+
+
