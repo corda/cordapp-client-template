137203304322e04f4495565dbf0fa97975fa2bff
Waldemar Zurowski <wzurowski@r3.com>
2020-02-10 15:30:45 +0000

    Updating Artifactory URLs
    * replaced ci-artifactory.corda.r3cev.com with software.r3.com

--- kubernetes/fetch_corda_jar.bat
+++ kubernetes/fetch_corda_jar.bat
@@ -1,4 +1,4 @@
 @echo off
-curl -o party-a/corda.jar https://ci-artifactory.corda.r3cev.com/artifactory/corda-releases/net/corda/corda/4.0/corda-4.0.jar
+curl -o party-a/corda.jar https://software.r3.com/artifactory/corda-releases/net/corda/corda/4.0/corda-4.0.jar
 copy party-a\corda.jar party-b\corda.jar 
 copy party-a\corda.jar party-c\corda.jar 
